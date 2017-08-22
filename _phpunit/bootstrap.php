3426f90f05c215f1c76417c2e5eee68fd3b06fad
Michael Teuscher <michael@scaly.ch>
2017-08-23 00:09:58 +0200

    Fixed php unit tests

--- _phpunit/bootstrap.php
+++ _phpunit/bootstrap.php
@@ -3,6 +3,7 @@
 include '../Core/ClassLoader/AutoLoader.php';
 define('SCALAR_CORE', dirname(getcwd()));
 
-$autoloader = Scalar\Core\ClassLoader\AutoLoader::getInstance();
+include SCALAR_CORE . '/Core/Scalar.php';
+$autoloader = new \Scalar\Core\ClassLoader\AutoLoader();
 $autoloader->register();
 $autoloader->addClassPath("Scalar\\", SCALAR_CORE);
