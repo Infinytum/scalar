609d72715dbe3b6d176cf6b1f336a9f17edc1cc2
Michael Teuscher <mk.teuscher@gmail.com>
2017-08-24 17:29:55 +0200

    Added License text to all files

--- IO/Exception/MalformedUriException.php
+++ IO/Exception/MalformedUriException.php
@@ -1,5 +1,25 @@
 <?php
 /**
+ * (C) 2017 by Michael Teuscher (mk.teuscher@gmail.com)
+ * as part of the Scalar PHP framework
+ *
+ * Released under the AGPL v3.0 license
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
  * Created by PhpStorm.
  * User: nila
  * Date: 19.05.17
